53ab3f64770e7865c5ec8dc8ad8756beae5f8412
sw <sean8987@gmail.com>
2020-10-02 23:59:31 +0900

    File Upload

---
+++ client-test/src/App.js
@@ -0,0 +1,63 @@
+import React, { useRef, useState } from 'react';
+import { post } from 'axios'
+
+function App(props) {
+  const [files, setFiles] = useState([]);
+  const imagesRef = useRef();
+  
+  // Upload to server
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    setFiles([]);
+    imagesRef.current.value = '';
+
+    const url = '/upload'
+    const formData = new FormData()
+    const config={ headers:{ 'content-type':'multipart/form-data' } }
+
+    for(var i=0; i<files.length;i++){
+      formData.append('image'+i,files[i]);
+    }
+
+    return post(url, formData, config)
+  }
+
+  // Image preview
+  const handleFileOnChange = (e) => {
+    e.preventDefault();
+    var newFileList = [];
+    const uploadedFile = e.target.files;
+    for (var i = 0; i < uploadedFile.length; i++) {
+      let reader = new FileReader();
+      let file = uploadedFile[i];
+      reader.onloadend = () => {
+        newFileList.push({
+          file: file,
+          previewURL: reader.result
+        })
+      }
+      reader.readAsDataURL(file);
+
+      if (i === uploadedFile.length - 1) {
+        setTimeout(() => setFiles(newFileList), 1000);
+      }
+    }
+  }
+  return (
+    <div>
+      <form onSubmit={e=>onSubmit(e)}>
+        <input type='file'
+          accept='image/jpg,impge/png,image/jpeg,image/gif'
+          name='profile_img'
+          onChange={handleFileOnChange}
+          ref={imagesRef}
+          multiple
+        />
+        <button type="submit">Upload</button>
+      </form>
+      {files.map(file => <img src={file.previewURL} width='100px' alt='profile_preview' />)}
+    </div>
+  );
+}
+
+export default App;
--- server/src/main/java/com/CIC/server/controller/CICController.java
+++ server/src/main/java/com/CIC/server/controller/CICController.java
@@ -3,18 +3,24 @@ package com.CIC.server.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
 import com.CIC.server.model.Project;
 import com.CIC.server.model.Type;
 import com.CIC.server.service.CICService;
-
+import com.CIC.server.service.FileUploadService;
 
 @RestController(value = "cicController")//<context:component-scan>
 public class CICController {
 	
 	@Autowired
-    private CICService cicService;  
+    private CICService cicService;
+	
+	@Autowired
+	FileUploadService fileUploadService;
 	
 	@GetMapping("/type")
     public List<Type> getTypeList() throws Exception { 
@@ -26,4 +32,25 @@ public class CICController {
         List<Project> list = this.cicService.getProjectList();
         return list; 
     }
+	
+	
+	@PostMapping("/upload")
+	public String fileUploadTest(
+			@RequestPart(value = "image0", required = false) MultipartFile file0,
+			@RequestPart(value = "image1", required = false) MultipartFile file1,
+			@RequestPart(value = "image2", required = false) MultipartFile file2,
+			@RequestPart(value = "image3", required = false) MultipartFile file3
+			) throws Exception {
+		
+//		String url0 = fileUploadService.restore(file0);
+//		String url1 = fileUploadService.restore(file1);
+//		String url2 = fileUploadService.restore(file2);
+//		String url3 = fileUploadService.restore(file3);
+		
+		System.out.println(file0);
+		System.out.println(file1);
+		System.out.println(file2);
+		System.out.println(file3);
+		return "result";
+	}
 }
