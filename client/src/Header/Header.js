497c6bd3867649b8427a860bb3babeae76509fc3
KJ <kjune4315@gmail.com>
2020-10-17 19:48:12 +0900

    2020-10-17 19:47 Change Header

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import Logo from './Logo'
+import React, { useContext } from 'react';
 import Styled from 'styled-components'; // Styled-components 라이브러리를 사용하기 위해 선언
+import Store from '../Store/Store';
+import Logo from './Logo'
 import SubMenu from './SubMenu';
 
 function Header() {
+  var idx = 0;
+  const { globalState } = useContext(Store);
+  const mainPage = ['all', 'tech', 'travel', 'fashion']; // Main Page Menu List
+  let mainPageSubMenut = '';
+  while (idx<mainPage.length){
+    if(mainPage[idx] === globalState.main){
+      mainPageSubMenut = <SubMenu/>
+      break;
+    }
+    idx += 1;
+}
   return (
     <Container>
           <Logo/>
-          <SubMenu/>
+          {mainPageSubMenut}
     </Container>
   );
 }
